4bd257b58e0841c2165696fc9c82fe546fc393d0
unknown <jjhernandez94@yahoo.com>
2018-02-13 19:00:40 -0800

    default movie functional

--- javascript/liri_new.js
+++ javascript/liri_new.js
@@ -22,57 +22,58 @@ var titleArray = [];
 var nodeArray = process.argv;
 var requestType = nodeArray[2];
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-function getTitle() {
-	for (var i = 3; i < nodeArray.length; i++) {
-		var titleSelection = nodeArray[i];
-		titleArray.push(titleSelection);
-		var getTitle = titleArray.toString();
-		
-	}	
-	
-	var title = getTitle.replace(",", " ");
-	
-	if (requestType === "movie-this") {
-		if (nodeArray === 3) {
-			var movieTitle = "Mr. Nobody";
-			getMovie(movieTitle);
-		}
-		else {
-			getMovie(title);
-		}
-	}
-	else if (requestType === "spotify-this-song") {
-		getSpotify(title);
-	}
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 switch (requestType) {
 	case "movie-this": 
-		getTitle();
+		getMovie();
 		break;
 	case "my-tweets":	
 		getTweet();
 		break;
 	case "spotify-this-song":
-		getTitle();
+		// getTitle();
+		getSpotify(word)
 		break;
 	case "do-what-it-says":
 
 		break;
 }//Switch closing brace
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-function getMovie(input) {	
-    // if (nodeArray.length === 3) {
-    // 	var title = "Mr. Nobody";
-    // }
-    // else {
+function getMovie() {	
+	console.log(nodeArray.length);
+	if (nodeArray.length === 3) {
 
+		var movie = "Mr.+Right";
 		// Then run a request to the OMDB API with the title specified
-		request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+		request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 	
 		    // If the request is successful (i.e. if the response status code is 200)
+		    if (!error && response.statusCode === 200) {
+		        console.log(JSON.parse(body).Title);
+		        console.log("Year of release: " + JSON.parse(body).Year);
+		        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+		        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+		        console.log("Produced in: " + JSON.parse(body).Country);
+		        console.log("Language: " + JSON.parse(body).Language);
+		        console.log("Plot: " + JSON.parse(body).Plot);
+		        console.log("Actors: " + JSON.parse(body).Actors);
+			}//if statement closing brace
+		});
+	}	
+	else {
+		var getTitle;
+		for (var i = 3; i < nodeArray.length; i++) {
+			var titleSelection = nodeArray[i];
+			titleArray.push(titleSelection);
+			getTitle = titleArray.toString();
+		}
+			getTitle.replace(",", " ");
+
+		var movie = getTitle;
+		// Then run a request to the OMDB API with the title specified
+		request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+	
 		    if (!error && response.statusCode === 200) {
 		        console.log("Movie title: " + JSON.parse(body).Title);
 		        console.log("Year of release: " + JSON.parse(body).Year);
@@ -83,9 +84,11 @@ function getMovie(input) {
 		        console.log("Plot: " + JSON.parse(body).Plot);
 		        console.log("Actors: " + JSON.parse(body).Actors);
 			}//if statement closing brace
-		});//request closing bracket
-	// }	
-}
+		});
+	}			
+}		
+// }//request closing bracket
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Used initially to create 20 tweets
 // var botPostCount = 0;
@@ -111,12 +114,15 @@ function getTweet() {
 function getSpotify(input) {
 	
 	spotifyClient.search({ type: 'track', query: input }, function(err, data) {	
+		
 		if (err) {
 			return console.log('Error occurred: ' + err);
 		}
-			console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
-			console.log("Track Title: " + data.tracks.items[0].name);
-			console.log("Preview Track: " + data.tracks.items[0].preview_url);
-			console.log("Album Title: " + data.tracks.items[0].album.name);
+		
+		console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+		console.log("Track Title: " + data.tracks.items[0].name);
+		console.log("Preview Track: " + data.tracks.items[0].preview_url);
+		console.log("Album Title: " + data.tracks.items[0].album.name);
+
 	});
 }
